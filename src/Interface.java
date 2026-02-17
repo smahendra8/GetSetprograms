818e3c4ae011c83f05985a15160eb10259510b03
Mahendra <sampathimahendra@gmail.com>
2026-02-17 17:56:17 +0530

    add few lines of code

--- src/Interface.java
+++ src/Interface.java
@@ -1,27 +1,56 @@
 public class Interface {
-    public static void main(String args[]){
-    interface I {
-        void print();
+    public static void main(String args[]) {
+        interface I {
+            void print();
 
-        int getValue();
-    }
-    class A implements I {
-        int value = -1;
-
-        @Override
-        public void print() {
-            System.out.println("A class print method");
+            int getValue();
         }
-        @Override
-        public int getValue() {
-            return value;
-        }
-    }
+        class A implements I {
+            int value = -1;
 
-        I a=new A();
+            @Override
+            public void print() {
+                System.out.println("A class print method");
+            }
+
+            @Override
+            public int getValue() {
+                return value;
+            }
+        }
+
+        I a = new A();
         a.print();
-        System.out.println("value is"+a.getValue());
+        System.out.println("value is" + a.getValue());
+        // System.out.println(a.Value);
 
+
+        class B {
+
+            I i;
+
+            B(I i) {
+                this.i = i;
+            }
+
+            void run() {
+                System.out.println(i.getValue());
+                i.print();
+                B b = new B(a);
+                b.run();
+
+
+
+
+
+            }
         }
     }
+    }
+
+
+
+
+
+
 
