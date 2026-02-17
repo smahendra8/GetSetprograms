9351cda1a3fa4a6cd24bc50162c9712e5405bfcc
Mahendra <sampathimahendra@gmail.com>
2026-02-17 15:16:25 +0530

    Initial Changes

---
+++ src/Getset.java
@@ -0,0 +1,21 @@
+ class Car {
+    private String name;
+        void start() {
+            System.out.println("car" + name + "is started");
+        }
+
+        void setnName(String name) {
+            this.name = name;
+        }
+
+        String getName() {
+            returnName;
+        }
+    }
+    Car car=new car();
+    car.setname("Audi");
+    Car.start();
+    system.out.println("car"+car getName()+"is ready to run);
+      car.setname("benz");
+    car.start();
+    system.ou.println("car"+car getName()+"is ready to run")
